7ea3ec0bf6c3767e2d113808610095bed6edbde0
Sky12072 <12072@coderacademy.edu.au>
2022-05-17 17:52:32 +1000

    show individual article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,34 @@ class ArticlesController < ApplicationController
     def index
         @time = Time.now
         @articles = Article.all
+
+        console
     end
+
+    def show
+        @article = Article.find(params[:id])
+
+        console
+    end
+
+    def new
+        @article = Article.new
+
+        console
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+        redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :importance, :body)
+    end
+
 end
