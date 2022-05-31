d1ba22aa617552711031c8b2d1214a07e0b4cbb0
Sky12072 <12072@coderacademy.edu.au>
2022-05-31 19:23:55 +1000

    update seed file to use a loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |i|
+    articles = Article.create([
+        { title: "This is title #{i}",
+            body: 'blabladlfal', 
+            importance: rand(1..10) 
+        }
+    ])
+end
